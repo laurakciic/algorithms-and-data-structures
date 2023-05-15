26ceda81330314333d661509d74a2f5930906ab9
Laura <laurakovacic@outlook.com>
2023-05-15 13:41:09 +0200

    add ArithmeticMean

---
+++ ArithmeticMean.java
@@ -0,0 +1,17 @@
+public class ArithmeticMean {
+    public static void main(String[] args) {
+        int[] testArray = {1, 2, 3, 4, 5};
+        System.out.println("Aritmeticka sredina: " + calculateArithmeticMean(testArray));
+    }
+
+    public static double calculateArithmeticMean(int[] array) {
+        if (array.length == 0) System.out.println("Empty array.");
+
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        return sum/2;
+    }
+}
