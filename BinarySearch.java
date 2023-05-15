680872a0dee61d4a645d37382cc8035595501ac1
Laura <laurakovacic@outlook.com>
2023-05-15 16:16:53 +0200

    add binary search

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] testArray = {1, 2, 3, 4, 5, 6, 10, 23, 32, 33, 34, 35, 36, 37};
+        int target = 36;
+        int foundElement = binarySearch(testArray, target);
+        System.out.println("Element " + foundElement + " found.");
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (array[mid] == target) {
+                return target;
+            } else if (array[mid] < target) {
+                left = mid + 1;                 // target je u desnoj polovici
+            } else {
+                right = mid - 1;                // target je u lijevoj polovici
+            }
+        }
+        return -1;
+    }
+}
