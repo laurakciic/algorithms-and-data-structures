ee19f6f44d47e688538858835a34d142a8082637
Laura <laurakovacic@outlook.com>
2023-05-15 16:21:13 +0200

    add binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,6 @@
 public class BinarySearch {
     public static void main(String[] args) {
+        // elements must be sorted
         int[] testArray = {1, 2, 3, 4, 5, 6, 10, 23, 32, 33, 34, 35, 36, 37};
         int target = 36;
         int foundElement = binarySearch(testArray, target);
